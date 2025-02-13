e6350bd30af8656ba99ab21727dc90f64d6c38a9
marteszibellina <valerababahov@yandex.ru>
2025-02-13 11:13:23 +0300

    Workflow #8: update

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,7 +1,10 @@
 # flake8: noqa
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 from django.core.management.utils import get_random_secret_key
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
