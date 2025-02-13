14764212ee0480b1e0c111a3ac24596c8bfd6f83
marteszibellina <valerababahov@yandex.ru>
2025-02-13 11:21:36 +0300

    Workflow #9: update

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -4,9 +4,9 @@ from pathlib import Path
 from dotenv import load_dotenv
 from django.core.management.utils import get_random_secret_key
 
-load_dotenv()
-
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR)
 
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
